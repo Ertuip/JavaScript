eed2efbb5c47f9ff203f75217eee65379bb962fa
Ertuip <141503250+Ertuip@users.noreply.github.com>
2023-09-04 19:31:52 -0300

    Deslocamento de Pastas

---
+++ Aulas/Aula 12 Exercícios/Projeto 1/script.js
@@ -0,0 +1,28 @@
+function load() {
+    var agora = new Date()
+    var hora = agora.getHours()
+    var min = agora.getMinutes()
+    var parag = document.getElementById('data')
+    var foto = document.getElementById('foto')
+
+    if (min < 10) {
+        min = '0' + min
+    }
+
+    if (hora == 0) {
+        parag.innerHTML = `Já é Meia Noite.`
+        foto.innerHTML = '<img src="img 1/Noite-1_3.png" alt="Foto de uma Noite">'
+    } else if (hora > 0 && hora < 6) {
+        parag.innerHTML = `Já são ${hora}:${min} da Madrugada`
+        foto.innerHTML = '<img src="img 1/Noite-1_3.png" alt="Foto de uma Noite">'
+    } else if (hora >= 6 && hora < 12) {
+        parag.innerHTML = `Agora São ${hora}:${min} Horas da Manhã.`
+        foto.innerHTML = '<img src="img 1/Manhã-1.png" alt="Foto de uma Manhã">'
+    } else if (hora >= 12 && hora < 18) {
+        parag.innerHTML = `Agora São ${hora}:${min} Horas da Tarde.`
+        foto.innerHTML = '<img src="img 1/Tarde-1.png" alt="Foto de uma Tarde">'
+    } else if (hora >= 18 && hora < 0) {
+        parag.innerHTML = `Já São ${hora}:${min} da Noite`
+        foto.innerHTML = '<img src="img 1/Noite-1_3.png" alt="Foto de uma Noite">'
+    }
+}
---
+++ Aulas/Aula 12/ex012.js
@@ -0,0 +1,16 @@
+var hora = 1
+if (hora = 1 ) {
+    console.log(`Agora são ${hora} hora.`)
+} else {
+    console.log(`Agora são ${hora} horas.`)
+}
+if (hora >= 23 && hora < 6) {
+    console.log('Vai dormir vei...')
+} else if (hora > 6 && hora < 12 ) {
+    console.log('Bom Dia!')
+} else if (hora >= 12 && hora < 18) {
+    console.log('Boa Tarde!')
+} else if (hora >= 18 && hora < 23) {
+    console.log('Boa Noite!')
+} 
+ 
