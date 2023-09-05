22a056456a6c1abf390af52766065b8fcaa1402f
Ertuip <141503250+Ertuip@users.noreply.github.com>
2023-09-05 20:17:22 -0300

    Finalização

--- Aulas/Aula 12 Exercícios/Projeto 1/script.js
+++ Aulas/Aula 12 Exercícios/Projeto 1/script.js
@@ -12,22 +12,27 @@ function load() {
     if (hora == 0) {
         parag.innerHTML = `Já é Meia Noite.`
         foto.innerHTML = '<img src="img 1/Noite-1_3.png" alt="Foto de uma Noite">'
-        document.body.style.background.color = '#515154'
+        document.body.style.backgroundColor = '#10152D'
+        document.body.style.textAlign = 'center'
     } else if (hora > 0 && hora < 6) {
         parag.innerHTML = `Já são ${hora}:${min} da Madrugada`
         foto.innerHTML = '<img src="img 1/Noite-1_3.png" alt="Foto de uma Noite">'
-        document.body.style.background.color = '#515154'
+        document.body.style.backgroundColor = '#10152D'
+        document.body.style.textAlign = 'center'
     } else if (hora >= 6 && hora < 12) {
         parag.innerHTML = `Agora São ${hora}:${min} Horas da Manhã.`
         foto.innerHTML = '<img src="img 1/Manhã-1.png" alt="Foto de uma Manhã">'
-        document.body.style.background.color = '#e2cd9f'
+        document.body.style.backgroundColor = '#e2cd9f'
+        document.body.style.textAlign = 'center'
     } else if (hora >= 12 && hora < 18) {
         parag.innerHTML = `Agora São ${hora}:${min} Horas da Tarde.`
         foto.innerHTML = '<img src="img 1/Tarde-1.png" alt="Foto de uma Tarde">'
-        document.body.style.background.color = '#b9846f'
-    } else if (hora >= 18 && hora < 0) {
+        document.body.style.backgroundColor = '#b9846f'
+        document.body.style.textAlign = 'center'
+    } else if (hora >= 18 && hora <= 23 && min <= 59){ // Alterado para else
         parag.innerHTML = `Já São ${hora}:${min} da Noite`
         foto.innerHTML = '<img src="img 1/Noite-1_3.png" alt="Foto de uma Noite">'
-        document.body.style.background.color = '#515154'
+        document.body.style.backgroundColor = '#10152D'
+        document.body.style.textAlign = 'center'
     }
 }
