a9e056f8b313f41bcd7f0ab06297396336d09388
Ertuip <141503250+Ertuip@users.noreply.github.com>
2023-09-06 20:34:24 -0300

    Criação para continuação depois

---
+++ Aulas/Aula 14 Exercícios/ex016/script.js
@@ -0,0 +1,11 @@
+function calc() {
+    var num = document.getElementById('inum').value
+    var num1 = document.getElementById('inum1').value
+    var pass = document.getElementById('ipas').value
+    var n = Number(num.value)
+    var n1 = Number(num1.value)
+    var pa = Number(pass.value)
+    var ste =
+    var passs = document.createElement = ('step')
+    passs.setAttribute('')
+}
