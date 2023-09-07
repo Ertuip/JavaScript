4c9292f583348362d124919c31f2c48a97839363
Ertuip <141503250+Ertuip@users.noreply.github.com>
2023-09-07 15:47:52 -0300

    Finalização do Exercício 16

--- Aulas/Aula 14 Exercícios/ex016/script.js
+++ Aulas/Aula 14 Exercícios/ex016/script.js
@@ -1,11 +1,32 @@
 function calc() {
-    var num = document.getElementById('inum').value
-    var num1 = document.getElementById('inum1').value
-    var pass = document.getElementById('ipas').value
-    var n = Number(num.value)
-    var n1 = Number(num1.value)
-    var pa = Number(pass.value)
-    var ste =
-    var passs = document.createElement = ('step')
-    passs.setAttribute('')
+    let num = document.getElementById('inum')
+    let num1 = document.getElementById('inum1')
+    let pass = document.getElementById('ipas')
+    let res = document.getElementById('res')
+    
+    if (num.value.length == 0 || num1.value.length == 0 || pass.value.length == 0){
+        alert('Você Precisa Escolher um Número Inicial, Final e os Passos da Contagem.')
+        return;
+    } 
+
+    let inicio = Number(num.value)
+    let fin = Number(num1.value)
+    let pas = Number(pass.value)
+    let c = inicio
+    res.innerHTML = '<br>Contando:<br><br> '
+
+    if (c < fin) {
+        do {
+            res.innerHTML += `${c} >> `
+            c += pas
+        } while (c < fin)
+    } 
+    
+    if (c > fin) {
+        do {
+            res.innerHTML += `${c} >> `
+            c -= pas
+        } while (fin <= c)
+    } 
+    res.innerHTML += ` ${c} >> <strong>Final!</strong>`
 }
